3a2ae39eb5dcda72c76652c061202f94b07ac1c8
CodeHex <x00.x7f@gmail.com>
2014-07-26 11:26:18 +0900

    Update death.pl

--- death.pl
+++ death.pl
@@ -7,10 +7,10 @@ use Encode qw/encode_utf8 decode_utf8/;
 
 my $twit = Net::Twitter->new(
    traits => ['API::RESTv1_1'],
-   consumer_key => 'PVonivgYA3JdQzhGY9SsfMoi0',
-   consumer_secret => 'VC5DHhuBFZI69asv3BIoFKbAYWfPYQOlGD0pv5uXmUCUd7iwky',
-   access_token => '1919043200-rajNa3iDzV8JWShbeE9tTmVo6ZDHP07ZtQ8RUPG',
-   access_token_secret => 'i36hiPz6eWdVVV1E797o4gRO0V2PelJAPfzGZbbDwmf6s',
+   consumer_key => 'API key',
+   consumer_secret => 'API Secret',
+   access_token => 'token',
+   access_token_secret => 'token secret',
    legacy_lists_api => 1,
    ssl => 1
 	);
