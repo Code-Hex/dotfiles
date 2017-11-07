72fcf3f836a9b6ca072488314239d27f12d51fdf
K <x00.x7f@gmail.com>
2017-11-07 15:35:45 +0900

    Modified prompt

--- zsh/.zsh/darwin/03_prompt.zsh
+++ zsh/.zsh/darwin/03_prompt.zsh
@@ -7,15 +7,15 @@ function _make_prompt() {
 
     local rand=${RANDOM}
     local colors=($(($rand % 7 + 1)), $((($rand + 1) % 7 + 1)), $((($rand + 2) % 7 + 1)))
-    local top="%K{black} %{$fg_bold[white]%}%n@%m %{$reset_color%}%k%K{132}%F{black}$_separator %f %{$fg_bold[white]%}%~ %{$reset_color%}%k%K{blue}%F{132}$_separator%f%k${vcs_info_msg_0_}"
+    local top="%n@%m %F{red}[%f%~%F{red}]%f ${vcs_info_msg_0_}"
     local bottom="%F{$colors[1]}❯%f%F{$colors[2]}❯%f%F{$colors[3]}❯%f "
     eval "$1='${top}${_newline}${bottom}'"
 }
 
 autoload -Uz vcs_info
 local branch=$'\ue0a0'
-zstyle ':vcs_info:*' formats "%K{blue} %F{black}$branch %b %f%k"
-zstyle ':vcs_info:*' actionformats "%K{blue} %F{black}$branch %b|%a %f%k"
+zstyle ':vcs_info:*' formats "%F{cyan}$branch %b %f"
+zstyle ':vcs_info:*' actionformats "%F{cyan}$branch %b|%a %f"
 precmd() {
     vcs_info
     _make_prompt PROMPT
