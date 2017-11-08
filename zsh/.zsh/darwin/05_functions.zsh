0a2508f4724b6bc8b6332b0fd8ffcf4d68bc506c
K <x00.x7f@gmail.com>
2017-11-08 18:59:24 +0900

    Modified tmux function

--- zsh/.zsh/darwin/05_functions.zsh
+++ zsh/.zsh/darwin/05_functions.zsh
@@ -28,22 +28,21 @@ reboot() { confirm_wrapper --root $0 "$@"; }
 poweroff() { confirm_wrapper --root $0 "$@"; }
 
 function run_under_tmux() {
-    # Run $1 under session or attach if such session already exist.
-    # $2 is optional path, if no specified, will use $1 from $PATH.
-    # If you need to pass extra variables, use $2 for it as in example below..
     # Example usage:
     #   torrent() { run_under_tmux 'rtorrent' '/usr/local/rtorrent-git/bin/rtorrent'; }
     #   mutt() { run_under_tmux 'mutt'; }
     #   irc() { run_under_tmux 'irssi' "TERM='screen' command irssi"; }
 
-
     # There is a bug in linux's libevent...
     # export EVENT_NOEPOLL=1
-
-    command -v tmux >/dev/null 2>&1 || return 1
+    if [ $TMUX = "" ]; then
+		echo "Error: cannot start command with new window from without tmux"
+		exit
+	fi
 
     if [ -z "$1" ]; then return 1; fi
-        local name="$1"
+    
+    local name="$1"
     if [ -n "$2" ]; then
         local execute="$2"
     else
@@ -53,8 +52,8 @@ function run_under_tmux() {
     if tmux has-session -t "${name}" 2>/dev/null; then
         tmux attach -d -t "${name}"
     else
-        tmux new-session -s "${name}" "${execute}" \; set-option status \; set set-titles-string "${name} (tmux@${HOST})"
+        tmux new-window -n "${name}" "${execute}"
     fi
 }
 
-irc() { run_under_tmux irssi "command irssi"; }
+irc() { run_under_tmux "irssi"; }
