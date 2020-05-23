49ed6436845deb9f8a7015b367ffa35ac5681e96
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-05-23 23:08:32 +0900

    fixed alias for vim

--- zsh/.zsh/darwin/02_alias.zsh
+++ zsh/.zsh/darwin/02_alias.zsh
@@ -1,13 +1,16 @@
 alias l='gls -GFT 0 --color=auto'
 alias ls='gls -aGFT 0 --color=auto'
 alias ll='ls -al'
-alias vim='/usr/local/bin/vim'
-alias vi='/usr/local/bin/vim'
 alias objdump='gobjdump' # objdump
 alias man="env LANG=C man" # man
 alias readelf='greadelf' # readelf
 alias gti='git'
+alias kubectl='kubectl.1.17'
 alias k='kubectl'
+
+if which lld >/dev/null; then
+    alias ld=lld
+fi
 
 alias subl='reattach-to-user-namespace subl' # subl for tmux
 alias code='reattach-to-user-namespace /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code' # code for tmux
@@ -16,3 +19,29 @@ alias code='reattach-to-user-namespace /Applications/Visual\ Studio\ Code.app/Co
 alias -s {pdf,png,jpg,bmp,PDF,PNG,JPG,BMP}='open -a Preview' # like ./filename.pdf
 alias -s html='open -a Google\ Chrome' # like ./page.html
 
+function _send_json_to_vim() {
+    if (( $# == 0 )) then
+        echo 'need a json as an argument'
+        return
+    fi
+    local json=$1
+    if ! jq -e . > /dev/null 2>&1 <<<"$json"; then
+        echo 'must specify json string'
+        return
+    fi
+    echo -e "\x1b]51;$json\x07"
+}
+
+function _vim() {
+    if [[ "$VIM_TERMINAL" ]] then
+        local args=$@
+        local paths=("$PWD" "${args[@]}")
+        local tapi_args=$(echo "$paths" | perl -pe 's/(.*?)\s+/"$1",/g' | perl -pe 's/,\z//')
+        _send_json_to_vim "[\"call\",\"Tapi_open\",[$tapi_args]]"
+        return
+    fi
+    /usr/local/bin/vim $@
+}
+
+alias vi='_vim'
+alias vim='_vim'
