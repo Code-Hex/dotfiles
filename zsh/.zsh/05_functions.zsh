7fab4e9e7ad9b8befca6eca66e5721134552a288
Kei Kamikawa <code-hex@users.noreply.github.com>
2021-08-27 13:03:46 +0900

    fixed to use fzf

--- zsh/.zsh/05_functions.zsh
+++ zsh/.zsh/05_functions.zsh
@@ -1,7 +1,7 @@
 function clr() {
-    for x in {0..8}; do 
-        for i in {30..37}; do 
-            for a in {40..47}; do 
+    for x in {0..8}; do
+        for i in {30..37}; do
+            for a in {40..47}; do
                 echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;38;40m "
             done
             echo
--- zsh/.zsh/06_peco.zsh
+++ zsh/.zsh/06_peco.zsh
@@ -1,23 +1,23 @@
-if which peco >/dev/null; then
+if which fzf >/dev/null; then
     function prkill() {
         # ps aux | peco | awk '{print $2}' | xargs kill -15
-        ps aux | peco | awk '{print $2}' | xargs kill -9
+        ps aux | fzf | awk '{print $2}' | xargs kill -9
     }
     function prterm() {
-        ps aux | peco | awk '{print $2}' | xargs kill -15
+        ps aux | fzf | awk '{print $2}' | xargs kill -15
     }
-    function exec_peco_history() {
-        BUFFER=$(history -n 1 | tail -r | awk '!a[$0]++' | peco)
+    function exec_fzf_history() {
+        BUFFER=$(history -n 1 | awk '!a[$0]++' | fzf +s --tac)
         CURSOR=$#BUFFER
         zle reset-prompt
     }
-    zle -N exec_peco_history
-    bindkey '^r' exec_peco_history
+    zle -N exec_fzf_history
+    bindkey '^r' exec_fzf_history
 
     # use ghq
     if which ghq >/dev/null; then
         function peco-ghq() {
-            local selected=$(ghq list -p | peco)
+            local selected=$(ghq list -p | fzf)
             if [ -n "$selected" ]; then
                 BUFFER="cd ${selected}"
                 zle accept-line
